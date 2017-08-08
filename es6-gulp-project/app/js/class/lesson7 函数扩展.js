5d02f25cf94de0e67e78b16ec3e1b86ee3a4aaa8
cg <654450188@qq.com>
2017-08-08 21:32:25 +0800

    函数扩展：参数默认值，函数作用域，rest参数，扩展运算符

---
+++ es6-gulp-project/app/js/class/lesson7 函数扩展.js
@@ -0,0 +1,49 @@
+{
+  /**
+   * 函数参数默认值
+   */
+  function test(x,y='world') {
+    //参数 y 默认值为 world
+    console.log('默认值', x, y);
+  }
+  test('hello') //hello world
+  test('hello', 'cg') //hello cg
+}
+
+{
+  /**
+   * 函数作用域
+   */
+  let x = 'test';
+  function test2(x, y=x) {
+    //给y赋值的x,是函数内部作用域的x
+    console.log('作用域',x,y);
+  }
+  test2('hello') //hello hello
+  test2() //undefined undefined
+}
+
+{
+  /**
+   * 函数的rest参数，类似 arguments 对象，
+   * 把离散的参数转化成伪数组
+   * 但是没有 arguments[0] 的问题
+   */
+  function test3(...arg) {
+    console.log(...arg); // 1 2 3 4 'a'
+    for (let v of arg) {
+      console.log('rest', v); // 1 2 3 4 'a'
+    }
+  }
+  test3(1,2,3,4,'a')
+}
+
+{
+  /**
+   * 扩展运算符，rest参数的逆运算
+   * 把数组参数转化成离散的参数
+   */
+  console.log(...[1,2,4]); //1 2 4
+  console.log('a', ...[1,2,4]); //a 1 2 4
+
+}
--- es6-gulp-project/app/js/index.js
+++ es6-gulp-project/app/js/index.js
@@ -2,4 +2,5 @@
 // import './class/lesson2 解构赋值.js';
 // import './class/lesson3 正则扩展.js';
 // import './class/lesson4 字符串扩展.js';
-import './class/lesson6 数组扩展.js'
+// import './class/lesson6 数组扩展.js';
+import './class/lesson7 函数扩展.js';
